e083a6629f031f28da2a3cfd787a07e45eaef9ff
Francois Berenger <berenger@riken.jp>
2013-05-16 17:18:21 +0900

    expose only what clients will need

--- genspir.mli
+++ genspir.mli
@@ -1,8 +1,2 @@
-val pi : float
-val compose : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
-val h : float -> float -> float
-val theta : float -> float -> float
-val psi : float -> float -> float -> float
-val genspir_spherical : float -> (float * float) list
-val to_xyz : float * float -> float * float * float
-val genspir_cartesian : float -> (float * float * float) list
+val genspir_spherical : int -> (float * float) list
+val genspir_cartesian : int -> (float * float * float) list
