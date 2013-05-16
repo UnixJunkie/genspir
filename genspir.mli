e8af95e991a0ce41c772121e8479b36c236cd373
Francois Berenger <berenger@riken.jp>
2013-05-16 17:10:07 +0900

    added, result of ocamlc -i genspir.ml > genspir.mli

---
+++ genspir.mli
@@ -0,0 +1,8 @@
+val pi : float
+val compose : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
+val h : float -> float -> float
+val theta : float -> float -> float
+val psi : float -> float -> float -> float
+val genspir_spherical : float -> (float * float) list
+val to_xyz : float * float -> float * float * float
+val genspir_cartesian : float -> (float * float * float) list
